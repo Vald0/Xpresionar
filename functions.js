d0e13e107e9f27a657eed6bbbd37a616fea3dc42
Nixnet <54565029+Vald0@users.noreply.github.com>
2020-12-30 20:55:06 -0600

    cosnole.log("Fuera") es borrado

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ document.addEventListener('click', function(event) {
     var isClickInsideElement = IgnorarElemento.contains(event.target);
     if (!isClickInsideElement) {
         hidebox.checked = false;
-        console.log("FUERA");
+
         //Do something click is outside specified element
     }
 });
